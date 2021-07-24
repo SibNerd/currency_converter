97dce8dba952eb0cc530f0b4da1d6b53ca402a64
Irina Yaremchuk <yaremiarem@gmail.com>
2021-07-24 13:16:39 +0000

    add convertion handling

--- lib/converter_web/live/converter_live.ex
+++ lib/converter_web/live/converter_live.ex
@@ -6,7 +6,20 @@ defmodule ConverterWeb.ConverterLive do
   @impl true
   def mount(_params, _session, socket) do
     currencies = Convert.get_currency_infos()
-    {:ok, assign(socket, currencies: currencies)}
+    socket = socket
+      |> assign(:currencies, currencies)
+      |> assign(:convertion_result, 0)
+    {:ok, socket}
+  end
+
+  @impl true
+  def handle_event("get_convertion", %{"currency_amount" => amount, "currency_name" => name}, socket) do
+    needed_currency = :ets.lookup(:currencies, name) |> List.first |> Tuple.to_list
+    [_ | info] = needed_currency
+    value = List.first(info) |> Map.fetch!("Value")
+    result = value * amount
+
+  {:noreply, assign(socket, convertion_result: result)}
   end
 
 end
